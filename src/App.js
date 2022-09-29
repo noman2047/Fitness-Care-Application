55f4050d9693307b5dd8f3f0eebcb2a47a667040
noman2047 <nomancse101@gmail.com>
2022-09-29 08:33:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-       <Shop></Shop>
     </div>
   );
 }
+
 export default App;
